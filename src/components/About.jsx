f1a1cb9e15313bff44ba860e21bae8d8cf25934c
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-08 21:41:29 +0545

    about feat:design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import profileImg from "../../public/sunilProfessional.jpg";
 
 const About = () => {
   return (
@@ -19,6 +20,51 @@ const About = () => {
         <p className="text-gray-400 text-center max-w-2xl mx-auto mb-16">
           Get to know more about my background and passion
         </p>
+        {/* image + my journey */}
+        <div className="flex flex-col md:flex-row items-center gap-12">
+          {/* image */}
+          <div className="md:w-1/2 rounded-2xl overflow-hidden">
+            <motion.img
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.9, ease: "easeOut" }}
+              viewport={{ once: false, amount: 0.2 }}
+              className="w-full h-full object-cover"
+              src={profileImg}
+              alt="Profile"
+            />
+          </div>
+          {/* Text content */}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.9, ease: "easeOut" }}
+            viewport={{ once: false, amount: 0.2 }}
+            className="md:w-1/2"
+          >
+            <div className="rounded-2xl p-8">
+              <h3 className="text-2xl font-semibold mb-6">My Journey</h3>
+              <p className="text-gray-300 mb-6">
+                I'm a passionate full-stack developer with over 5 years of
+                experience creating digital solutions for businesses around the
+                world. My journey started with basic HTML/CSS websites and has
+                evolved into building complex web applications with modern
+                frameworks.
+              </p>
+              <p className="text-gray-300 mb-12">
+                When I’m not coding, you can find me exploring new technologies,
+                contributing to open-source projects, or sharing my knowledge
+                through tech blogs and tutorials. I believe in continuous
+                learning and pushing the boundaries of what’s possible on the
+                web.
+              </p>
+              {/* cards */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {aboutInfo.map}
+              </div>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </motion.div>
   );
