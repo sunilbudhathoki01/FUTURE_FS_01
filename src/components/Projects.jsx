6eeade1a0df15d537ab3c932a845a95b682cf4a9
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-12-11 20:50:53 +0545

    feat:projects page design complete

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { projects } from "../assets/assets";
+import ProjectCard from "./ProjectCard";
+import { FaArrowRight } from "react-icons/fa";
 
 const Projects = () => {
   return (
@@ -18,8 +21,22 @@ const Projects = () => {
         <p className="text-gray-400 text-center max-w-2xl mx-auto mb-16 ">
           A selection of my recent work
         </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+          {/* project card */}
+          {projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
+        <div className="text-center mt-12 ">
+          <a
+            href="#"
+            className="inline-flex items-center px-6 py-3 border border-purple-600 rounded-lg font-medium hover:bg-purple-700/20 transition duration-300"
+          >
+            <span className="">View More Projects</span>
+            <FaArrowRight className="ml-2" />
+          </a>
+        </div>
       </div>
-      <h1></h1>
     </motion.div>
   );
 };
